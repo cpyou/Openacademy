3380c89b167699031a4805ecf9fc70cf3d7a1ed6
cpyou <745690939@qq.com>
2014-03-08 19:24:15 +0800

    Add wizard action_add_attendee function and modify view

--- chen_openacademy/wizard/create_attendee.py
+++ chen_openacademy/wizard/create_attendee.py
@@ -4,6 +4,17 @@ from osv import osv, fields
 class create_attendee_wizard(osv.TransientModel):
     _name = 'openacademy.create.attendee.wizard'
 
+    def action_add_attendee(self, cr, uid, ids, context=None):
+        attendee_model = self.pool.get('openacademy.attendee')
+        wizard = self.browse(cr, uid, ids[0], context=context)
+        for attendee in wizard.attendee_ids:
+            attendee_model.create(cr, uid, {
+                'partner_id': attendee.partner_id.id,
+                'session_id': wizard.session_id.id,
+            })
+        return {}
+        #return {'type': 'ir.actions.act_window.close'}
+
     _columns = {
         'session_id': fields.many2one('openacademy.session', 'Session',
                                       required=True),
