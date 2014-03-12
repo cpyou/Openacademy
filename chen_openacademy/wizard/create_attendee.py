68578ac272db1695882fa329f6b6ca222a7248f0
cpyou <745690939@qq.com>
2014-03-12 23:40:51 +0800

    modify wizard

--- chen_openacademy/wizard/create_attendee.py
+++ chen_openacademy/wizard/create_attendee.py
@@ -7,7 +7,7 @@ class create_attendee_wizard(osv.TransientModel):
     def action_add_attendee(self, cr, uid, ids, context=None):
         session_model = self.pool.get('openacademy.session')
         wizard = self.browse(cr, uid, ids[0], context=context)
-        session_ids = [wizard.session_id.id]
+        session_ids = [sess.id for sess in wizard.session_ids]
         att_data = [{'partner_id': att.partner_id.id for att in wizard.attendee_ids}]
         session_model.write(cr, uid, session_ids,
                             {'attendee_ids': [(0, 0, data) for data in att_data]},
@@ -15,11 +15,20 @@ class create_attendee_wizard(osv.TransientModel):
         return {}
         #return {'type': 'ir.actions.act_window.close'}
 
+    def _get_active_session(self, cr, uid, context):
+        if context.get('active_model') == 'openacademy.session':
+            return context.get('active_id', False)
+        return False
+
     _columns = {
-        'session_id': fields.many2one('openacademy.session', 'Session',
-                                      required=True),
+        'session_ids': fields.many2many('openacademy.session', 'Session',
+                                        required=True),
         'attendee_ids': fields.one2many('openacademy.attendee.wizard',
                                         'wizard_id', 'Attendees'),
+    }
+
+    _defaults = {
+        'session_ids': _get_active_session,
     }
 
 
